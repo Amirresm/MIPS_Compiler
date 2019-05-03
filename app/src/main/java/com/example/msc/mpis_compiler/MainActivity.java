0325a4ee91450edda2908c31c3b7f417e1db2405
Amirreza Esmaeili <amirreza.77.esmaeili@gmail.com>
2019-05-03 18:47:15 +0430

    file saving more error handling bug fixes

--- app/src/main/java/com/example/msc/mpis_compiler/MainActivity.java
+++ app/src/main/java/com/example/msc/mpis_compiler/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.msc.mpis_compiler;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,10 +12,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.example.msc.mpis_compiler.containers.CompileState;
 import com.example.msc.mpis_compiler.listeners.EditorTextWatcher;
 import com.example.msc.mpis_compiler.containers.MapsContainer;
 import com.example.msc.mpis_compiler.utilities.Utility;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -26,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     EditText codeEt;
     TextView outputTV;
 
+    Uri filePath;
 
     public static MapsContainer maps;
     public static CompileState state = new CompileState();
@@ -43,7 +50,12 @@ public class MainActivity extends AppCompatActivity {
         newBt = findViewById(R.id.new_bt);
         compileBt = findViewById(R.id.compile_bt);
         codeEt = findViewById(R.id.code_et);
-        outputTV= findViewById(R.id.output_tv);
+        outputTV = findViewById(R.id.output_tv);
+
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                1);
 
         fileBt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 // Set your required file type
                 intent.setType("*/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Choose file..."),1001);
+                startActivityForResult(Intent.createChooser(intent, "Choose file..."), 1001);
             }
         });
 
@@ -62,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 codeEt.setText("");
                 outputTV.setText("");
-                Utility.resetAll(maps,state);
+                Utility.resetAll(maps, state);
             }
         });
 
@@ -71,12 +83,16 @@ public class MainActivity extends AppCompatActivity {
         compileBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(state.errors.isEmpty()) {
-                    String output = Utility.mainScan(maps, state, codeEt.getText().toString());
+                if (codeEt.getEditableText().toString().isEmpty())
+                    return;
+                if (state.errors.isEmpty()) {
+                    final String output = Utility.mainScan(maps, state, codeEt.getText().toString());
                     outputTV.setText(output);
-                }
-                else
-                    Toast.makeText(MainActivity.this, "Please fix the following errors before compiling: " + Utility.reportErrors(state),Toast.LENGTH_LONG).show();
+
+                    String fileName = filePath.getPath().substring(filePath.getPath().lastIndexOf("/") + 1);
+                    Utility.saveFile(MainActivity.this, output, fileName);
+                } else
+                    Toast.makeText(MainActivity.this, "Please fix the following errors before compiling: " + Utility.reportErrors(state), Toast.LENGTH_LONG).show();
             }
         });
 
@@ -89,27 +105,45 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == 1001) {
             try {
-                InputStream fis = getContentResolver().openInputStream(data.getData());
-                BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
-                StringBuilder sb = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null)
-                {
-                    sb.append(line).append("\n");
+                if (data != null) {
+                    filePath = data.getData();
+                    InputStream fis = getContentResolver().openInputStream(filePath);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line).append("\n");
+                    }
+                    codeEt.setText(sb.toString());
+//                    Toast.makeText(this, "Code loaded.", Toast.LENGTH_SHORT).show();
                 }
-                codeEt.setText(sb.toString());
-                //Utility.firstScan(maps, state, sb.toString());
-                Toast.makeText(this, "Code loaded.", Toast.LENGTH_SHORT).show();
             } catch (FileNotFoundException e) {
-                Toast.makeText(this, "File not found!", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "File not found!", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             } catch (IOException e) {
-                Toast.makeText(this, "Code format is wrong!", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Code format is wrong!", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             } catch (Exception e) {
-                Toast.makeText(MainActivity.this, "Code syntax error: " + e, Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Code syntax error: " + e, Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
-        }}
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage; Cannot save outputs!", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
 
 }
